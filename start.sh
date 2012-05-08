7d283853697915a6bf1b5090ab9374e5fc661c94
zhengtao.wuzt <zhengtao.wuzt@gmail.com>
2012-05-08 13:11:36 +0800

    add log

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # NOTE: mustache templates need \ because they are not awesome.
-exec erl -pa ebin edit deps/*/ebin  apps/*/ebin  -boot start_sasl \
+exec erl -pa  deps/*/ebin  apps/*/ebin  -boot start_sasl \
     -setcookie aaa \
     -s erlmc \
     +K true +P 10240000 \
